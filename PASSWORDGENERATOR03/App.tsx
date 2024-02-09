9319ed55117cf9d59779d6f52b36bc2570633848
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-09 13:08:12 +0530

    Lecture - 23

--- PASSWORDGENERATOR03/App.tsx
+++ PASSWORDGENERATOR03/App.tsx
@@ -19,8 +19,14 @@ const App = () => {
     //
   };
   const createPassword = (characters: string, passwordLength: number) => {
-    //
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   };
+  const resetPassword = () => {};
   return (
     <View>
       <Text>Apps</Text>
