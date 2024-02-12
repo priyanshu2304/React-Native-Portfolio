14dd4fbc66ab6a5d7c6abefdb726a7a99d2d3fd6
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-12 07:12:19 +0530

    Lecture - 29, Handling Images.

---
+++ rollthedice04/src/App.tsx
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  ImageSourcePropType,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>hey</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+});
+
+export default App;
