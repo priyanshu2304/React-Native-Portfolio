a906d17409c559f810c943bd97ed03a4c70927c2
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-14 07:20:08 +0530

    Lecture - 37, Installed FontAwesome Icons

---
+++ ticTacToe07/components/icons.tsx
@@ -0,0 +1,21 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+type IconsProps = PropsWithChildren<{
+  name: string;
+}>;
+const icons = ({name}: IconsProps) => {
+  switch (name) {
+    case 'circle':
+      return <Icon name="circle-thin" size={38} color="#f7cd2e" />;
+    case 'cross':
+      return <Icon name="times" size={38} color="#38cc77" />;
+    default:
+      return <Icon name="pencil" size={38} color="#0d0d0d" />;
+  }
+};
+
+export default icons;
