1afab1817809fb5eb0e503e86fddabf27f85822d
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-18 20:07:27 +0530

    Lecture - 42, Creating a music server

---
+++ Spotify08/src/App.tsx
@@ -0,0 +1,14 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
