e8f41ea9fbb91f0f589f8469008ab14d98941a03
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-19 19:10:55 +0530

    Lecture - 43, Conditional Rendering UI for Music

--- Spotify08/src/App.tsx
+++ Spotify08/src/App.tsx
@@ -1,7 +1,33 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {setupPlayer, addTrack} from '../musicPlayerServices';
 const App = () => {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+  async function setup() {
+    let isSetup = await setupPlayer();
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  }
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
   return (
     <SafeAreaView>
       <Text>App</Text>
@@ -11,4 +37,6 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
