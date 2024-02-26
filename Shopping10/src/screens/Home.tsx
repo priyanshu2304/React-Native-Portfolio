d8c09d8a372ee195e315d7805a3e14f75b5ee063
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-26 20:06:19 +0530

    Lecture - 54

--- Shopping10/src/screens/Home.tsx
+++ Shopping10/src/screens/Home.tsx
@@ -1,14 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const Home = () => {
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import ProductItem from '../components/ProductItem';
+import Seprator from '../components/Seprator';
+import {PRODUCTS_LIST} from '../data/constants';
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+const Home = ({navigation}: HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={PRODUCTS_LIST}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={Seprator}
+        renderItem={({item}) => (
+          <Pressable
+            onPress={() =>
+              navigation.navigate('Details', {
+                product: item,
+              })
+            }>
+            <ProductItem product={item} />
+          </Pressable>
+        )}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+});
