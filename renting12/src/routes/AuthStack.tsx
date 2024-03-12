3766e3e7d1ae9ddc678f12210d545e9a0a5df120
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-03-12 20:15:40 +0530

    Added Cart Screen

--- renting12/src/routes/AuthStack.tsx
+++ renting12/src/routes/AuthStack.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 import SignUp from '../screens/SignUp';
 import Login from '../screens/Login';
+import Cart from '../screens/Cart';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 export type AuthStackParamList = {
   SignUp: undefined;
   Login: undefined;
+  Cart: undefined;
   Home: undefined;
 };
 
@@ -20,6 +22,7 @@ const AuthStack = () => {
       }}>
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Cart" component={Cart} />
     </Stack.Navigator>
   );
 };
